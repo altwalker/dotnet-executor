4324e3b4965e8c98246f1eaefe02b95496fe5f0a
Robert-96 <dezmereanrobert@gmail.com>
2023-09-15 11:53:55 +0100

    Update integration tests

--- AltwalkerExecutor.Tests/Integration/TestRestApi.cs
+++ AltwalkerExecutor.Tests/Integration/TestRestApi.cs
@@ -20,89 +20,104 @@ namespace Tests.Integration
         [OneTimeSetUp]
         public void Init()
         {
-            Console.WriteLine("OneTimeSetUp");
             executorService = new ExecutorService();
             executorService.RegisterModel<ModelExample>();
+
             baseUri = new Uri("http://localhost:5037/");
             Console.WriteLine(baseUri.ToString());
+
             executorService.Start(new string[] { "--server.urls=" + baseUri.ToString() });
             httpClient = new HttpClient();
         }
 
-        [Test]
-        public async Task Test_Rest_UnhandledUrls()
+        [OneTimeTearDown]
+        public void Cleanup()
         {
-            var response = await httpClient.GetAsync(baseUri + "someRandomPath");
+            executorService.StopAsync().GetAwaiter().GetResult();
+        }
 
-            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+        public async Task Get(str url)
+        {
+            return await httpClient.GetAsync(baseUri + uri);
+        }
+
+        public async Task Post(str url, str content)
+        {
+            return await httpClient.PostAsync(baseUri + url, new StringContent(content, Encoding.UTF8, "application/json"));
+        }
+
+        public async Task Put(str url, str content)
+        {
+            return await httpClient.PostAsync(baseUri + url, new StringContent(content, Encoding.UTF8, "application/json"));
+        }
+
+        public async dynamic GetJSON(Task response)
+        {
             var content = await response.Content.ReadAsStringAsync();
             var json = JsonConvert.DeserializeObject<dynamic>(content);
+
+            return json;
+        }
+
+        [Test]
+        public async Task TestUnhandledUrls()
+        {
+            var response = Get("someRandomPath");
+            var json = GetJSON(response);
+
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
             Assert.IsNotNull(json);
             Assert.AreEqual("Handler not found", json["error"]["message"].Value);
         }
 
         [Test]
-        public async Task Test_Rest_ModelNotFound()
+        public async Task TestModelNotFound()
         {
-            var response = await httpClient.PostAsync(baseUri + "altwalker/executeStep?modelName=invalidmodel&name=step",
-                new StringContent(string.Empty, Encoding.UTF8, "application/json"));
+            var response = Post("altwalker/executeStep?modelName=InvalidModel&name=step", string.Empty);
+            var json = GetJSON(response);
+
             Assert.AreEqual(460, (int)response.StatusCode);
-            var content = await response.Content.ReadAsStringAsync();
-            var json = JsonConvert.DeserializeObject<dynamic>(content);
             Assert.IsNotNull(json);
-            Assert.AreEqual("No model named `invalidmodel` was registered", json["error"]["message"].Value);
+            Assert.AreEqual("No model named `InvalidModel` was registered", json["error"]["message"].Value);
         }
 
         [Test]
-        public async Task Test_Rest_StepNotFound()
+        public async Task TestStepNotFound()
         {
-            var response = await httpClient.PostAsync(baseUri + "altwalker/executeStep?modelName=ModelExample&name=invalidStep",
-                new StringContent(string.Empty, Encoding.UTF8, "application/json"));
+            var response = Post("altwalker/executeStep?modelName=ModelExample&name=invalidStep", string.Empty);
+            var json = GetJSON(response);
 
             Assert.AreEqual(461, (int)response.StatusCode);
-            var content = await response.Content.ReadAsStringAsync();
-            var json = JsonConvert.DeserializeObject<dynamic>(content);
             Assert.IsNotNull(json);
             Assert.AreEqual("Method named `invalidStep` not found in class `ModelExample`.", json["error"]["message"].Value);
         }
 
-
         [Test]
-        public async Task Test_Rest_InvalidHandler()
+        public async Task TestInvalidHandler()
         {
-            var response = await httpClient.PostAsync(baseUri + "altwalker/executeStep?modelName=ModelExample&name=invalid_handler",
-                new StringContent(string.Empty, Encoding.UTF8, "application/json"));
-
-            var content = await response.Content.ReadAsStringAsync();
-            var json = JsonConvert.DeserializeObject<dynamic>(content);
+            var response = Post("altwalker/executeStep?modelName=ModelExample&name=invalid_handler", string.Empty);
+            var json = GetJSON(response);
 
             Assert.AreEqual(462, (int)response.StatusCode);
             Assert.IsNotNull(json);
             Assert.AreEqual("InvalidHandler for `invalid_handler` in type `ModelExample`.", json["error"]["message"].Value);
         }
 
-        public async dynamic PostExecuteStep(str url, str content) {
-            var response = await httpClient.PostAsync(baseUri + url, new StringContent(content, Encoding.UTF8, "application/json"));
-            var responseContent = await response.Content.ReadAsStringAsync();
-            var json = JsonConvert.DeserializeObject<dynamic>(responseContent);
-
-            return json;
-        }
-
         [Test]
-        public async Task Test_Rest_MultipleHandlers()
+        public async Task TestMultipleHandlers()
         {
-            var json = PostExecuteStep("altwalker/executeStep?modelName=ModelExample&name=multiple_overloads", string.Empty);
+            var json = GetJSON(Post("altwalker/executeStep?modelName=ModelExample&name=multiple_overloads", string.Empty));
 
             Assert.IsNotNull(json);
             Assert.AreEqual("MultipleHandlers for `multiple_overloads` in type `ModelExample`.", json["error"]["message"].Value);
         }
 
         [Test]
-        public async Task Test_Rest_ExecuteStepWithData()
+        public async Task TestExecuteStepWithData()
         {
             var content = @"{""data"": { ""key"":""value"" }}";
-            var json = PostExecuteStep("altwalker/executeStep?modelName=ModelExample&name=handler_with_data", content);
+            var response = Post("altwalker/executeStep?modelName=ModelExample&name=handler_with_data", content);
+            var json = GetJSON(response);
 
             Assert.AreEqual(200, (int)response.StatusCode);
             Assert.That(json.payload.data["key"].Value, Is.EqualTo("value"));
@@ -111,44 +126,31 @@ namespace Tests.Integration
         }
 
         [Test]
-        public async Task Test_Rest_ExecuteStepWithContext()
+        public async Task TestExecuteStepWithContext()
         {
-            // handler_with_return_value
-            // return "message from the other side";
-            var response = await httpClient.PostAsync(baseUri + "altwalker/executeStep?modelName=ModelExample&name=handler_with_return_value",
-                new StringContent(string.Empty, Encoding.UTF8, "application/json"));
-
-            var responseContent = await response.Content.ReadAsStringAsync();
-
-            var json = JsonConvert.DeserializeObject<dynamic>(responseContent);
+            var response = Post("altwalker/executeStep?modelName=ModelExample&name=handler_with_return_value", string.Empty);
+            var json = GetJSON(response);
 
             Assert.AreEqual(200, (int)response.StatusCode);
             Assert.That(json.payload.result.Value, Is.EqualTo("message from the other side"));
         }
 
         [Test]
-        public async Task Test_Rest_Load()
+        public async Task TestLoad()
         {
             var content = @"{""path"": ""somepath""";
-            var response = await httpClient.PostAsync(baseUri + "altwalker/load",
-                new StringContent(content, Encoding.UTF8, "application/json"));
+            var response = Post("altwalker/load", content);
+
             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
         }
 
         [Test]
-        public async Task Test_Rest_Reset()
+        public async Task TestReset()
         {
             var content = @"{""path"": ""somepath""";
-            var response = await httpClient.PutAsync(baseUri + "altwalker/reset",
-                new StringContent(content, Encoding.UTF8, "application/json"));
+            var response = Put("altwalker/reset", content);
+
             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-        }
-
-
-        [OneTimeTearDown]
-        public void Cleanup()
-        {
-            executorService.StopAsync().GetAwaiter().GetResult();
         }
     }
 
